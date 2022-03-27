3ab1736bb554156b0d39342505d8bf26d05356fe
redBrigid <brigidnkuo@gmail.com>
2022-03-27 17:13:41 +0200

    "Numerical Palindrome #1" - js

---
+++ 7kyu/Numerical_Palindrome_1.js
@@ -0,0 +1,22 @@
+// A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward as forward. Examples of numerical palindromes are:
+
+// 2332
+// 110011
+// 54322345
+
+// For a given number num, write a function to test if it's a numerical palindrome or not and return a boolean (true if it is and false if not).
+
+// Return "Not valid" if the input is not an integer or less than 0.
+
+// Other Kata in this Series:
+// Numerical Palindrome #1
+// Numerical Palindrome #1.5
+// Numerical Palindrome #2
+// Numerical Palindrome #3
+// Numerical Palindrome #3.5
+// Numerical Palindrome #4
+// Numerical Palindrome #5
+
+function palindrome(num) {
+    return (typeof(num) === 'number' && num > 0) ? parseInt(num) == num.toString().split('').reverse().join('') : 'Not valid'
+}
