5bfd2e2230bc5c682bbe300a096f57b48de9e9fa
redBrigid <brigidnkuo@gmail.com>
2022-04-07 14:13:28 +0200

    "ASCII Shift Encryption/Decryption" -js

---
+++ 7kyu/Ascii_Shift_Encryption_Decryption.js
@@ -0,0 +1,31 @@
+// Ascii Shift Encryption/Decryption
+// The goal of this kata is to create a very simple ASCII encryption and decryption. The encryption algorithm should shift each character's charcode by the character's current index in the string (0-based).
+
+// The input strings will never require to go outside of the ASCII range.
+
+// Example:
+//   p | a | s | s | w | o | r | d # Plaintext
+// + 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 # Shift (add)
+//   p | b | u | v | [ | t | x | k # Ciphertext
+// The decryption should reverse this:
+
+//   p | b | u | v | [ | t | x | k # Ciphertext
+// - 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 # Shift (subtract)
+//   p | a | s | s | w | o | r | d # Plaintext
+// ALGORITHMSASCIICHARACTER ENCODINGSFORMATS
+
+function asciiEncrypt(plaintext) {
+    let result = ''
+    for (let i = 0; i < plaintext.length; i++) {
+        result += String.fromCharCode(plaintext.charCodeAt(i) + i)
+    };
+    return result
+};
+
+function asciiDecrypt(ciphertext) {
+    let result = ''
+    for (let i = 0; i < ciphertext.length; i++) {
+        result += String.fromCharCode(ciphertext.charCodeAt(i) - i)
+    };
+    return result
+};
